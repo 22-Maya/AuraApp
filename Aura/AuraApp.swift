36841fda348aa4d9663314ceb268a0e1209d8b64
22-Maya <maya.itskovich@gmail.com>
2025-06-04 17:50:45 -0400

    Aura Application
    Day 1 file upload

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Student on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aura/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Aura
+//
+//  Created by Student on 6/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Welcome \nTo Aura")
+                .font(.system(size: 60))
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 69/255.0, green: 54/255.0, blue: 89/255.0))
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // Make VStack fill available space
+        .background(
+            RadialGradient(
+                gradient: Gradient(colors: [
+                    Color(red: 235/255.0, green: 236/255.0, blue: 255/255.0),
+                    //Color(red: 164/255.0, green: 173/255.0, blue: 224/255.0),
+                    Color(red: 141/255.0, green: 140/255.0, blue: 207/255.0)]),
+                center: .center, // Specify the center point
+                startRadius: 50, // Inner radius
+                endRadius: 200 // Outer radius
+            )
+            .ignoresSafeArea() // Make the gradient ignore safe areas
+        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
