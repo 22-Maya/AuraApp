6c8b40d6ea67b9b25d29febf77b8af88fe51a56e
22-Maya <124386537+22-Maya@users.noreply.github.com>
2025-06-04 20:56:51 -0400

    Update ContentView.swift
    added a button to navigate to the main page

--- Aura/ContentView.swift
+++ Aura/ContentView.swift
@@ -9,29 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack {
-            Text("Welcome \nTo Aura")
-                .font(.system(size: 60))
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color(red: 69/255.0, green: 54/255.0, blue: 89/255.0))
-        	Button("Enter")
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity) // Make VStack fill available space
-        .background(
-            RadialGradient(
-                gradient: Gradient(colors: [
-                    Color(red: 235/255.0, green: 236/255.0, blue: 255/255.0),
-                    //Color(red: 164/255.0, green: 173/255.0, blue: 224/255.0),
-                    Color(red: 141/255.0, green: 140/255.0, blue: 207/255.0)]),
-                center: .center, // Specify the center point
-                startRadius: 50, // Inner radius
-                endRadius: 200 // Outer radius
+        NavigationView {
+            VStack {
+                Text("Welcome \nTo Aura")
+                    .font(.system(size: 60))
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(red: 69/255.0, green: 54/255.0, blue: 89/255.0))
+            	NavigationLink(destination: HomePageView()) {
+                    Text("Enter")
+                        .font(.system(size: 30))
+                        .background(Color: .white)
+                        .cornerRadius(25)
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity) // Make VStack fill available space
+            .background(
+                RadialGradient(
+                    gradient: Gradient(colors: [
+                        Color(red: 235/255.0, green: 236/255.0, blue: 255/255.0),
+                        //Color(red: 164/255.0, green: 173/255.0, blue: 224/255.0),
+                        Color(red: 141/255.0, green: 140/255.0, blue: 207/255.0)]),
+                    center: .center, // Specify the center point
+                    startRadius: 50, // Inner radius
+                    endRadius: 200 // Outer radius
+                )
+                .ignoresSafeArea() // Make the gradient ignore safe areas
             )
-            .ignoresSafeArea() // Make the gradient ignore safe areas
-        )
+        }
     }
 }
 
