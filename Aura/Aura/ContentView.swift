052d3052b2596f69dee53eaaedb06f3bfa3bfb8e
22-Maya <maya.itskovich@gmail.com>
2025-06-10 22:03:31 -0400

    Starting ios app

---
+++ Aura/Aura/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Aura
+//
+//  Created by Student on 6/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            ZStack(alignment: .bottom) {
+                VStack {
+                    Text("Welcome \nTo Aura")
+                        .font(.system(size: 60, weight: .semibold, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(Color(red: 69/255.0, green: 54/255.0, blue: 89/255.0))
+                }
+                .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    RadialGradient(
+                        gradient: Gradient(colors: [
+                            Color(red: 235/255.0, green: 236/255.0, blue: 255/255.0),
+                            Color(red: 141/255.0, green: 140/255.0, blue: 207/255.0)]),
+                        center: .center,
+                        startRadius: 50,
+                        endRadius: 200
+                    )
+                )
+                .ignoresSafeArea()
+                
+                NavigationLink(destination: HomePageView()) {
+                    Text("Enter")
+                        .padding()
+                        .font(.system(size: 40, design: .serif))
+                        .background(Color(red: 235/255.0, green: 236/255.0, blue: 255/255.0))
+                        .cornerRadius(25)
+                        .foregroundStyle(Color(red: 69/255.0, green: 54/255.0, blue: 89/255.0))
+                }
+                .padding(.bottom, 20)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
